f0ae954fffdba68fd5e35bc0ce6324a2a9e5d2ad
Seijo Cecilia <willamwei@outlook.com>
2025-05-08 00:09:44 +0800

    [milky] update description for no_cache

--- src/main/kotlin/org/ntqqrev/saltify/milky/common/CachedActionBase.kt
+++ src/main/kotlin/org/ntqqrev/saltify/milky/common/CachedActionBase.kt
@@ -4,7 +4,7 @@ import org.ntqqrev.saltify.composeidl.BooleanType
 import org.ntqqrev.saltify.composeidl.Struct
 
 val CacheActionBase = Struct {
-    field("no_cache", BooleanType, "不使用缓存") {
+    field("no_cache", BooleanType, "是否强制不使用缓存") {
         default("false")
     }
 }
