0d4bad62c01174c6be7c960d83256b5b3a1bb9df
Seijo Cecilia <willamwei@outlook.com>
2025-05-10 14:21:26 +0800

    [milky] move CachedApiBase into SystemApi.kt

--- src/main/kotlin/org/ntqqrev/saltify/milky/api/SystemApi.kt
+++ src/main/kotlin/org/ntqqrev/saltify/milky/api/SystemApi.kt
@@ -1,8 +1,9 @@
 package org.ntqqrev.saltify.milky.api
 
+import org.ntqqrev.saltify.composeidl.BooleanType
 import org.ntqqrev.saltify.composeidl.Category
+import org.ntqqrev.saltify.composeidl.Struct
 import org.ntqqrev.saltify.composeidl.UIntType
-import org.ntqqrev.saltify.milky.common.CachedApiBase
 import org.ntqqrev.saltify.milky.common.FriendEntity
 import org.ntqqrev.saltify.milky.common.GroupEntity
 import org.ntqqrev.saltify.milky.common.GroupMemberEntity
@@ -10,9 +11,15 @@ import org.ntqqrev.saltify.milky.common.GroupMemberEntity
 val SystemApi = Category("system") {
     describe("系统 API")
 
+    val cachedApiBase = Struct {
+        field("no_cache", BooleanType, "是否强制不使用缓存") {
+            default("false")
+        }
+    }
+
     api("get_friend_list") {
         describe("获取好友列表")
-        input(CachedApiBase)
+        input(cachedApiBase)
         outputArray(FriendEntity)
     }
 
@@ -20,14 +27,14 @@ val SystemApi = Category("system") {
         describe("获取好友信息")
         input {
             field("user_id", UIntType, "好友 QQ 号")
-            use(CachedApiBase)
+            use(cachedApiBase)
         }
         output(FriendEntity)
     }
 
     api("get_group_list") {
         describe("获取群列表")
-        input(CachedApiBase)
+        input(cachedApiBase)
         outputArray(GroupEntity)
     }
 
@@ -35,7 +42,7 @@ val SystemApi = Category("system") {
         describe("获取群信息")
         input {
             field("group_id", UIntType, "群号")
-            use(CachedApiBase)
+            use(cachedApiBase)
         }
         output(GroupEntity)
     }
@@ -44,7 +51,7 @@ val SystemApi = Category("system") {
         describe("获取群成员列表")
         input {
             field("group_id", UIntType, "群号")
-            use(CachedApiBase)
+            use(cachedApiBase)
         }
         outputArray(GroupMemberEntity)
     }
@@ -54,7 +61,7 @@ val SystemApi = Category("system") {
         input {
             field("group_id", UIntType, "群号")
             field("user_id", UIntType, "群成员 QQ 号")
-            use(CachedApiBase)
+            use(cachedApiBase)
         }
         output(GroupMemberEntity)
     }
