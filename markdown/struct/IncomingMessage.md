7a00d0ad0310691c222dd486580171acc16e70f9
Wesley F. Young <wesley.f.young@outlook.com>
2025-05-19 19:28:36 +0800

    [milky] add more meta info into IncomingMessage

--- markdown/struct/IncomingMessage.md
+++ markdown/struct/IncomingMessage.md
@@ -1,11 +1,32 @@
 # 接收消息
+共有字段如下：
+
 | 字段名 | 类型 | 描述 |
 | --- | --- | --- |
-| message_scene | string | 消息场景（可能值：`friend`, `group`, `temp`） |
 | peer_id | int64 | 好友 QQ 号或群号 |
 | message_seq | int64 | 消息序列号 |
 | sender_id | int64 | 发送者 QQ 号 |
 | time | int64 | 消息 Unix 时间戳（秒） |
 | segments | Array<[IncomingSegment](../struct/IncomingSegment.md)> | 消息段列表 |
-| client_seq | int64 | 私聊消息的客户端序列号（**可选**） |
+| message_scene | string | 类型标识符 |
+
+不同的 message_scene 值对应不同的附加字段，具体如下：
+
+## `friend` 好友消息
+
+| 字段名 | 类型 | 描述 |
+| --- | --- | --- |
+| friend | [Friend](../struct/Friend.md) | 好友信息 |
+| client_seq | int64 | 客户端序列号 |
+## `group` 群消息
+
+| 字段名 | 类型 | 描述 |
+| --- | --- | --- |
+| group | [Group](../struct/Group.md) | 群信息 |
+| group_member | [GroupMember](../struct/GroupMember.md) | 群成员信息 |
+## `temp` 临时会话消息
+
+| 字段名 | 类型 | 描述 |
+| --- | --- | --- |
+| sender_group | [Group](../struct/Group.md) | 临时会话发送者的所在的群信息（**可选**） |
 
--- src/main/kotlin/org/ntqqrev/milky/message/IncomingMessage.kt
+++ src/main/kotlin/org/ntqqrev/milky/message/IncomingMessage.kt
@@ -1,16 +1,35 @@
 package org.ntqqrev.milky.message
 
+import org.ntqqrev.milky.common.FriendEntity
+import org.ntqqrev.milky.common.GroupEntity
+import org.ntqqrev.milky.common.GroupMemberEntity
 import org.ntqqrev.saltify.composeidl.Array
 import org.ntqqrev.saltify.composeidl.LongType
-import org.ntqqrev.saltify.composeidl.Struct
-import org.ntqqrev.milky.common.MessageIdentifier
+import org.ntqqrev.saltify.composeidl.DiscriminatedUnion
 
-val IncomingMessage = Struct("IncomingMessage") {
+val IncomingMessage = DiscriminatedUnion("IncomingMessage", "message_scene") {
     describe("接收消息")
 
-    use(MessageIdentifier)
+    field("peer_id", LongType, "好友 QQ 号或群号")
+    field("message_seq", LongType, "消息序列号")
     field("sender_id", LongType, "发送者 QQ 号")
     field("time", LongType, "消息 Unix 时间戳（秒）")
     field("segments", Array(IncomingSegment), "消息段列表")
-    field("client_seq", LongType, "私聊消息的客户端序列号") { optional() }
+
+    struct("friend") {
+        describe("好友消息")
+        field("friend", FriendEntity, "好友信息")
+        field("client_seq", LongType, "客户端序列号")
+    }
+
+    struct("group") {
+        describe("群消息")
+        field("group", GroupEntity, "群信息")
+        field("group_member", GroupMemberEntity, "群成员信息")
+    }
+
+    struct("temp") {
+        describe("临时会话消息")
+        field("sender_group", GroupEntity, "临时会话发送者的所在的群信息") { optional() }
+    }
 }
