8ad19c696450b117d1ec82d8863bff0203244c39
Wesley F. Young <wesley.f.young@outlook.com>
2025-05-27 20:43:37 +0800

    [milky] add `sex` attribute to Friend

--- markdown/struct/Friend.md
+++ markdown/struct/Friend.md
@@ -4,6 +4,7 @@
 | user_id | int64 | 好友 QQ 号 |
 | qid | string | 好友 QID（**可选**） |
 | nickname | string | 好友昵称 |
+| sex | enum (string) | 性别，可能值：`male`, `female`, `unknown` |
 | remark | string | 好友备注 |
 | category | [FriendCategory](../struct/FriendCategory.md) | 好友分组（**可选**） |
 
--- src/main/kotlin/org/ntqqrev/milky/common/FriendEntity.kt
+++ src/main/kotlin/org/ntqqrev/milky/common/FriendEntity.kt
@@ -9,6 +9,9 @@ val FriendEntity = Struct("Friend") {
     field("user_id", LongType, "好友 QQ 号")
     field("qid", StringType, "好友 QID") { optional() }
     field("nickname", StringType, "好友昵称")
+    field("sex", StringType, "性别") {
+        enum("male", "female", "unknown")
+    }
     field("remark", StringType, "好友备注")
     field("category", FriendCategoryEntity, "好友分组") { optional() }
 }
