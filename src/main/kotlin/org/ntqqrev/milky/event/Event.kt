25c09eb129be4a05e0c0dd7fa931d3eebd37bced
Wesley F. Young <wesley.f.young@outlook.com>
2025-05-29 10:02:44 +0800

    [milky] move MessageRecallEvent

--- src/main/kotlin/org/ntqqrev/milky/event/Event.kt
+++ src/main/kotlin/org/ntqqrev/milky/event/Event.kt
@@ -14,43 +14,24 @@ val Event = DiscriminatedUnion("Event", "event_type") {
     field("self_id", LongType, "机器人 QQ 号")
 
     struct("message_receive", IncomingMessage)
-
-    struct("message_recall") {
-        describe("撤回消息")
-        use(MessageIdentifier)
-        field("sender_id", LongType, "被撤回的消息的发送者 QQ 号")
-        field("operator_id", LongType, "操作者 QQ 号")
-    }
+    struct("message_recall", MessageRecallEvent)
 
     struct("friend_request", FriendRequestEvent)
-
     struct("group_join_request", GroupJoinRequestEvent)
-
     struct("group_invited_join_request", GroupInvitedJoinRequestEvent)
-
     struct("group_invitation_request", GroupInvitationRequestEvent)
 
     struct("friend_nudge", FriendNudgeEvent)
-
     struct("friend_file_upload", FriendFileUploadEvent)
 
     struct("group_admin_change", GroupAdminChangeEvent)
-
     struct("group_essence_message_change", GroupEssenceMessageChangeEvent)
-
     struct("group_member_increase", GroupMemberIncreaseEvent)
-
     struct("group_member_decrease", GroupMemberDecreaseEvent)
-
     struct("group_name_change", GroupNameChangeEvent)
-
     struct("group_message_reaction", GroupMessageReactionEvent)
-
     struct("group_mute", GroupMuteEvent)
-
     struct("group_whole_mute", GroupWholeMuteEvent)
-
     struct("group_nudge", GroupNudgeEvent)
-
     struct("group_file_upload", GroupFileUploadEvent)
 }
