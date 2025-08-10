4052178615a3db2688c753a84eff9ba33b33c5a1
Wesley F. Young <wesley.f.young@outlook.com>
2025-08-10 08:57:36 +0800

    fix: font reference & location

--- packages/website/app/layout.jsx
+++ packages/website/app/layout.jsx
@@ -1,7 +1,15 @@
 import { Layout, Navbar } from 'nextra-theme-docs';
 import { getPageMap } from 'nextra/page-map';
 import 'nextra-theme-docs/style.css';
-import '@fontsource-variable/inter';
+import '@fontsource/inter/100.css';
+import '@fontsource/inter/200.css';
+import '@fontsource/inter/300.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
+import '@fontsource/inter/800.css';
+import '@fontsource/inter/900.css';
 import '@fontsource-variable/noto-sans-sc';
 import './styles.css';
 import { commonStructs } from '@/component/StructRenderer';
