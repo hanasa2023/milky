a4a5e7dcf4980e27305536c7288c4f849a9c2570
Wesley F. Young <wesley.f.young@outlook.com>
2025-09-02 13:19:23 +0800

    feat(website): add raw generated json schema

--- packages/website/app/common.ts
+++ packages/website/app/common.ts
@@ -1,1 +1,25 @@
-export * from '@saltify/milky-types/api'
+import { z, ZodVoid } from 'zod';
+import { milkyVersion, milkyPackageVersion } from '@saltify/milky-types';
+import { commonStructs, apiCategories } from '@saltify/milky-types/api';
+
+Object.entries(commonStructs).forEach(([name, schema]) => {
+  z.globalRegistry.add(schema, { id: name });
+});
+
+Object.entries(apiCategories).forEach(([, category]) => {
+  category.apis.forEach((api) => {
+    z.globalRegistry.add(api.inputStruct, { id: `Api_${api.endpoint}_input` });
+    if (!(api.outputStruct instanceof ZodVoid)) {
+      z.globalRegistry.add(api.outputStruct, { id: `Api_${api.endpoint}_output` });
+    }
+  });
+});
+
+export * from '@saltify/milky-types/api';
+export const jsonSchemas = {
+  milkyVersion: milkyVersion,
+  packageVersion: milkyPackageVersion,
+  schemas: z.toJSONSchema(z.globalRegistry, {
+    metadata: z.globalRegistry,
+  }).schemas,
+};
---
+++ packages/website/app/raw/json-schema/route.ts
@@ -0,0 +1,9 @@
+import { jsonSchemas } from '../../common';
+
+export const dynamic = 'force-static';
+
+export function GET() {
+  return new Response(JSON.stringify(jsonSchemas), {
+    status: 200,
+  });
+}
