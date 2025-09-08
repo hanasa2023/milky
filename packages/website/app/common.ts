4ba56dd694347c13bf377b703fb9bbcf8776259f
舰队的偶像-岛风酱! <frg2089@outlook.com>
2025-09-08 15:28:25 +0800

    refactor(types): 重构 API 类型定义和结构 (#21)
    - 将 commonStructs 从对象改为数组，存储类型名称
    - 修改 Api 接口，inputStruct 和 outputStruct 改为类型名称
    - 更新 apiCategories 中的 API 定义，使用新的类型名称
    - 在 website 项目中实现新的 API 结构
    
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- packages/website/app/common.ts
+++ packages/website/app/common.ts
@@ -1,6 +1,38 @@
-import { z, ZodVoid } from 'zod';
-import { milkyVersion, milkyPackageVersion } from '@saltify/milky-types';
-import { commonStructs, apiCategories } from '@saltify/milky-types/api';
+import { z, ZodType, ZodVoid } from 'zod';
+import * as types from '@saltify/milky-types';
+import * as apis from '@saltify/milky-types/api';
+
+export const commonStructs = Object.fromEntries(apis.commonStructs.map((i) => [i, types[i]])) as Record<
+  string,
+  ZodType
+>;
+
+export interface Api {
+  endpoint: string;
+  description: string;
+  inputStruct: ZodType;
+  outputStruct: ZodType;
+}
+
+export interface ApiCategory {
+  name: string;
+  apis: Api[];
+}
+
+export const apiCategories = Object.fromEntries(
+  Object.entries(apis.apiCategories).map(([categoryName, categoryInfo]) => [
+    categoryName,
+    {
+      name: categoryInfo.name,
+      apis: categoryInfo.apis.map((api) => ({
+        endpoint: api.endpoint,
+        description: api.description,
+        inputStruct: api.inputStruct ? types[api.inputStruct] : z.object({}),
+        outputStruct: api.outputStruct ? types[api.outputStruct] : z.void(),
+      })),
+    },
+  ])
+) as Record<string, ApiCategory>;
 
 Object.entries(commonStructs).forEach(([name, schema]) => {
   z.globalRegistry.add(schema, { id: name, description: schema.description });
@@ -17,8 +49,8 @@ Object.entries(apiCategories).forEach(([, category]) => {
 
 export * from '@saltify/milky-types/api';
 export const jsonSchemas = {
-  milkyVersion: milkyVersion,
-  packageVersion: milkyPackageVersion,
+  milkyVersion: types.milkyVersion,
+  packageVersion: types.milkyPackageVersion,
   schemas: z.toJSONSchema(z.globalRegistry, {
     metadata: z.globalRegistry,
   }).schemas,
